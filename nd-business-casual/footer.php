d27dc7093fce7401a2cdb8f08f1874341b5eb44a
Cristopher Ocaña <crissoca@gmail.com>
2015-01-28 12:46:03 -0600

    Working on the blog view

--- nd-business-casual/footer.php
+++ nd-business-casual/footer.php
@@ -8,15 +8,21 @@
  */
 ?>
 
-	</div><!-- #content -->
+  </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'business-casual' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'business-casual' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'business-casual' ), 'Business Casual', '<a href="http://newdev.co/" rel="designer">Crissoca</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <p>
+            <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'business-casual' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'business-casual' ), 'WordPress' ); ?></a>
+            <span class="sep"> | </span>
+            <?php printf( __( 'Theme: %1$s by %2$s.', 'business-casual' ), 'Business Casual', '<a href="http://newdev.co/" rel="designer">Crissoca</a>' ); ?>
+          </p>
+        </div>
+      </div>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- nd-business-casual/index.php
+++ nd-business-casual/index.php
@@ -13,34 +13,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <div id="primary" class="content-area">
+    <main class="row">
+      <div id="main" class="site-main col-md-8 box" role="main">
+        <?php if ( have_posts() ) : ?>
+          <?php /* Start the Loop */ ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+            <?php
+              /* Include the Post-Format-specific template for the content.
+               * If you want to override this in a child theme, then include a file
+               * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+               */
+              get_template_part( 'content', get_post_format() );
+            ?>
+          <?php endwhile; ?>
+          <?php business_casual_paging_nav(); ?>
+        <?php else : ?>
+          <?php get_template_part( 'content', 'none' ); ?>
+        <?php endif; ?>
+      </div>
+      <?php get_sidebar(); ?>
+    </main><!-- #main -->
 
-		<?php if ( have_posts() ) : ?>
+  </div><!-- #primary -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php business_casual_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
